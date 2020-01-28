b075df70111d745f86fbc554abae13b1b60202d5
Chu Chang Ku <cm1cck@sheffield.ac.uk>
2020-01-28 16:56:08 +0000

    Edit spaces

--- R/compile_model.R
+++ R/compile_model.R
@@ -15,31 +15,34 @@
 #' @examples
 compile_likefree_model <- function(dat, m_sim, y0, rprior, dprior, times=dat[, "t"],
                                    m_warmup=NA, t_warmup=100) {
-  
+
   sim <- list(data=dat, y0=y0,
               r_prior=rprior, d_prior=dprior,
               m_sim=m_sim, m_warmup=m_warmup)
-  
-  
+
+
   stopifnot(is.finite(dprior(rprior())))
-  
+
   if (is.na(m_warmup)) {
     sim$cm_warmup <- NA
     sim$ts_warmup <- NA
   } else {
     env <- r_prior()
     env$y0 <- y0
-    
+
     sim$cm_warmup <- m_warmup(user=env)
     sim$ts_warmup <- seq(min(times) - t_warmup, min(times), by=1)
   }
-  
+
   env <- r_prior()
   env$y0 <- y0
-  
+
   sim$cm_sim <- m_sim(user=env)
   sim$ts_sim <- sort(times)
-  
+
+  sim$ts_to_fit <- sort(intersect(dat[, "t"], sim$ts_sim))
+  sim$cols_to_fit <- colnames(dat)[-1]
+
   class(sim) <- "likefree_model"
   return(sim)
 }
